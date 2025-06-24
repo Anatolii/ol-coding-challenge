40f7f7f00bfb8ec39af10b7bbbf0d52467ac9c98
Anatolii Isaiev <anatolii.isaiev@gmail.com>
2025-06-24 04:43:09 -0700

    UI improvements

--- UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/ui/screen/PhotosScreen.kt
+++ UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/ui/screen/PhotosScreen.kt
@@ -1,12 +1,21 @@
 package dev.anatolii.unsplashcuratedphotos.ui.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.navigation3.ListDetailSceneStrategy
 import androidx.compose.material3.adaptive.navigation3.rememberListDetailSceneStrategy
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation3.runtime.entry
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
@@ -15,14 +24,34 @@ import dev.anatolii.unsplashcuratedphotos.model.PhotosScreenViewModel
 import dev.anatolii.unsplashcuratedphotos.ui.component.DetailsViewPlaceholder
 import dev.anatolii.unsplashcuratedphotos.ui.component.PhotosGrid
 
-@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@OptIn(ExperimentalMaterial3AdaptiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun PhotosScreen(modifier: Modifier = Modifier, viewModel: PhotosScreenViewModel) {
 
     val backStack = rememberNavBackStack(PhotosGridNavKey)
     val listDetailStrategy = rememberListDetailSceneStrategy<Any>()
 
-    Scaffold(modifier = modifier) { paddingValues ->
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        text = stringResource(dev.anatolii.unsplashcuratedphotos.R.string.app_name),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                    )
+                },
+                modifier = Modifier,
+            )
+        }
+    ) { paddingValues ->
         NavDisplay(
             backStack = backStack,
             modifier = Modifier.padding(paddingValues),
