8d5805ecb55488c189f78346e188ef65b88be020
Anatolii Isaiev <anatolii.isaiev@gmail.com>
2025-06-24 04:42:29 -0700

    Use AsyncImage to show photos in the grid

--- UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/ui/component/PhotoGridItem.kt
+++ UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/ui/component/PhotoGridItem.kt
@@ -4,43 +4,36 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import dev.anatolii.unsplashcuratedphotos.R
 import dev.anatolii.unsplashcuratedphotos.ui.data.Photo
-import dev.anatolii.unsplashcuratedphotos.ui.utils.ColorSaver
 
 @Composable
 fun PhotoGridItem(modifier: Modifier = Modifier, photo: Photo, onItemClick: (Photo) -> Unit = {}) {
-    var backgroundColor by rememberSaveable(saver = ColorSaver()) { mutableStateOf(Color.White) }
     Surface(
         modifier = modifier
             .fillMaxSize()
-            .background(backgroundColor)
-            .focusable(enabled = true)
-            .onFocusChanged {
-                backgroundColor = if (it.isFocused) Color.LightGray else Color.White
-            },
+            .background(Color.White)
+            .focusable(enabled = true),
         tonalElevation = 16.dp,
         onClick = {
             onItemClick(photo)
         }
     ) {
-        Text(
-            text = photo.id,
-            modifier = modifier
-                .background(Color.Yellow)
-                .wrapContentSize()
+        AsyncImage(
+            model = photo.thumbnailUrl,
+            contentDescription = photo.altDescription,
+            modifier = Modifier.fillMaxSize().padding(4.dp),
+            placeholder = painterResource(R.drawable.ic_launcher_foreground),
+            error = painterResource(R.drawable.ic_launcher_background),
+            fallback = painterResource(R.drawable.ic_launcher_foreground),
         )
     }
 }
