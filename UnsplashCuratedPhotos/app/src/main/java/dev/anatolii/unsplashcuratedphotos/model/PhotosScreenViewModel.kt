8bf3c540bb11310a7cd5b71dd30db167ced8c7c5
Anatolii Isaiev <anatolii.isaiev@gmail.com>
2025-06-24 02:42:46 -0700

    Add ViewModel that can be used by the photos list screen

---
+++ UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/model/PhotosScreenViewModel.kt
@@ -0,0 +1,30 @@
+package dev.anatolii.unsplashcuratedphotos.model
+
+import androidx.lifecycle.ViewModel
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import dev.anatolii.unsplashcuratedphotos.network.Constants
+import dev.anatolii.unsplashcuratedphotos.ui.data.Photo
+import kotlinx.coroutines.flow.Flow
+
+class PhotosScreenViewModel: ViewModel() {
+
+    @OptIn(ExperimentalPagingApi::class)
+    private val unsplashPhotosPager: Pager<Int, Photo> by lazy{ Pager<Int, Photo>(
+        config = PagingConfig(
+            pageSize = Constants.PhotosApi.PER_PAGE_MAX_VALUE,
+            initialLoadSize = Constants.PhotosApi.PER_PAGE_MAX_VALUE,
+            enablePlaceholders = true,
+            maxSize = Constants.PhotosApi.PAGE_MAX_VALUE * Constants.PhotosApi.PER_PAGE_MAX_VALUE,
+        ),
+        initialKey = 1,
+        remoteMediator = null, // to be used to implement local database caching
+        pagingSourceFactory = {
+            UnsplashPhotosPagingSourceFactory()
+        }
+    ) }
+
+    val photos: Flow<PagingData<Photo>> = unsplashPhotosPager.flow
+}
