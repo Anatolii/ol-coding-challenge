850e4af25387ef2f33c40f59279c7fa9bc655b2f
Anatolii Isaiev <anatolii.isaiev@gmail.com>
2025-06-25 01:31:55 -0700

    Add KDoc to PhotoAdapter

--- UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/model/util/PhotoAdapter.kt
+++ UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/model/util/PhotoAdapter.kt
@@ -2,7 +2,16 @@ package dev.anatolii.unsplashcuratedphotos.model.util
 
 import dev.anatolii.unsplashcuratedphotos.network.data.Photo
 
+/**
+ * Adapter allows converting between Photo data representations.
+ */
 class PhotoAdapter {
+
+    /**
+     * Converts a Photo from the network representation to the UI representation
+     * @param networkPhoto the Photo from the network
+     * @return the Photo in the UI representation
+     */
     fun adaptToUi(networkPhoto: Photo): dev.anatolii.unsplashcuratedphotos.ui.data.Photo {
         return dev.anatolii.unsplashcuratedphotos.ui.data.Photo(
             id = networkPhoto.id,
