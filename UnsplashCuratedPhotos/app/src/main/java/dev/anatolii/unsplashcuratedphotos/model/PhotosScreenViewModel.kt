64639bc7fce1c9c5a7e9b092697939dd6a7d7710
Anatolii Isaiev <anatolii.isaiev@gmail.com>
2025-06-25 01:32:49 -0700

    Add docs to PhotosScreenViewModel

--- UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/model/PhotosScreenViewModel.kt
+++ UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/model/PhotosScreenViewModel.kt
@@ -12,26 +12,41 @@ import dev.anatolii.unsplashcuratedphotos.network.Constants
 import dev.anatolii.unsplashcuratedphotos.ui.data.Photo
 import kotlinx.coroutines.flow.Flow
 
-class PhotosScreenViewModel: ViewModel() {
+/**
+ * The view model for the photos screen
+ */
+class PhotosScreenViewModel : ViewModel() {
 
     @OptIn(ExperimentalPagingApi::class)
-    private val unsplashPhotosPager: Pager<Int, Photo> by lazy{ Pager<Int, Photo>(
-        config = PagingConfig(
-            pageSize = Constants.PhotosApi.PER_PAGE_MAX_VALUE,
-            initialLoadSize = Constants.PhotosApi.PER_PAGE_MAX_VALUE,
-            enablePlaceholders = true,
-            maxSize = Constants.PhotosApi.PAGE_MAX_VALUE * Constants.PhotosApi.PER_PAGE_MAX_VALUE,
-        ),
-        initialKey = 1,
-        remoteMediator = null, // to be used to implement local database caching
-        pagingSourceFactory = {
-            UnsplashPhotosPagingSourceFactory()
-        }
-    ) }
+    private val unsplashPhotosPager: Pager<Int, Photo> by lazy {
+        Pager<Int, Photo>(
+            config = PagingConfig(
+                pageSize = Constants.PhotosApi.PER_PAGE_MAX_VALUE,
+                initialLoadSize = Constants.PhotosApi.PER_PAGE_MAX_VALUE,
+                enablePlaceholders = true,
+                maxSize = Constants.PhotosApi.PAGE_MAX_VALUE * Constants.PhotosApi.PER_PAGE_MAX_VALUE,
+            ),
+            initialKey = 1,
+            remoteMediator = null, // to be used to implement local database caching
+            pagingSourceFactory = {
+                UnsplashPhotosPagingSourceFactory()
+            }
+        )
+    }
 
+    /**
+     * Photos that can be used in a lazy loading composables.
+     * Calls [androidx.paging.cachedIn] with [androidx.lifecycle.viewModelScope] parameter before returning
+     * the [Flow] to ensure that it is preserved between configuration changes, until the view model is active.
+     *
+     * @return the flow of photos
+     */
     val photos: Flow<PagingData<Photo>> by lazy {
         unsplashPhotosPager.flow.cachedIn(viewModelScope)
     }
 
+    /**
+     * The position of the selected photo.
+     */
     val selectedPhotoPosition: MutableLiveData<Int> = MutableLiveData<Int>(-1)
 }
