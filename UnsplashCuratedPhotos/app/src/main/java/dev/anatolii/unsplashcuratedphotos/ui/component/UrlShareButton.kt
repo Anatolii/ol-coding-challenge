62baa5191595d4b6f6796190f054e021803c2cd9
Anatolii Isaiev <anatolii.isaiev@gmail.com>
2025-06-24 19:16:53 -0700

    Add Url share button
    To be used in Photo Detail screen.
    It will allow to share the Unsplash URL for a displayed photo.

---
+++ UnsplashCuratedPhotos/app/src/main/java/dev/anatolii/unsplashcuratedphotos/ui/component/UrlShareButton.kt
@@ -0,0 +1,32 @@
+package dev.anatolii.unsplashcuratedphotos.ui.component
+
+import android.content.Intent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import dev.anatolii.unsplashcuratedphotos.R
+
+@Composable
+fun UrlShareButton(modifier: Modifier, url: String) {
+    val context = LocalContext.current
+    Button(
+        onClick = {
+            val sendIntent = Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_TEXT, url)
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            context.startActivity(shareIntent)
+        },
+        modifier = modifier,
+    ) {
+        Icon(
+            painter = painterResource(android.R.drawable.ic_menu_share),
+            contentDescription = stringResource(R.string.share_url_button_icon_description)
+        )
+    }
+}
